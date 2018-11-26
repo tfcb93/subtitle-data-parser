e6a867bb45ff6fb659b0866e821766e2198fe584
Thiago Borges <thiagoborges533@gmail.com>
2018-11-26 21:13:35 -0200

    Sobe com a versão mais atual do subtitle-data-parser

---
+++ Subtitle.js
@@ -0,0 +1,50 @@
+class Subtitle{
+    constructor(totalSubtitles){
+        this.subs = totalSubtitles //recebe uma lista com todas as legendas encontradas
+        this.greatLineNumber = this.greatLineNum(totalSubtitles)
+        this.maxIndex = this.setMaxIndex(totalSubtitles)
+        this.mediumDur = this.calcMediumDur(totalSubtitles)
+    }
+
+    /**
+     * greatLineNum retorna o maior número de linhas de todas as entradas
+     * esse valor é importante para a criação do CSV
+     */
+    greatLineNum(subs){
+        let max = 0
+        subs.forEach((l,key)=>{
+            if(l.lines.length > max){
+                max = l.lines.length
+            }
+        })
+        return max
+    }
+    /**
+     * Retorna o maior index da lista de entradas
+     */
+    setMaxIndex(subs){
+        return subs[(subs.length - 1)].index
+    }
+    /**
+     * Retorna o tempo médio de duração para o arquivo inteiro
+     */
+    calcMediumDur(subs){
+        let m = 0
+        subs.forEach((l,key)=>{
+            m = m + l.dur
+        })
+        return m / (subs[(subs.length - 1)].index)
+    }
+
+    allToCSV(){
+        let out = []
+        this.subs.forEach((l,key)=>{
+            out.push(l.csvString(this.greatLineNumber))
+        })
+        return out
+    }
+}
+
+
+
+module.exports = Subtitle
---
+++ SubtitleInput.js
@@ -0,0 +1,85 @@
+class SubtitleInput {
+        constructor(index, sTime, eTime, lines){
+            this.index = parseInt(index)
+            this.sTime = this.timeToMs(sTime)
+            this.eTime = this.timeToMs(eTime)
+            this.dur = this.durCalc(this.sTime, this.eTime)
+            this.lines = lines
+            this.charsPerLine = this.charPerLineCalc(lines)
+            this.charsPerSec = this.charsPerSecCalc(this.charsPerLine,this.dur)
+        }
+        durCalc(startTime,endTime){
+            return endTime - startTime
+        }
+
+        charPerLineCalc(lines){
+            let chars = []
+            lines.forEach((line,key) => {
+                chars.push(line.length)
+            })
+            return chars
+        }
+
+        charsPerSecCalc(cpLine, dur){
+            let totalChar = 0
+            cpLine.forEach((line,key) => {
+                totalChar = totalChar + line
+            })
+            return totalChar / (dur/1000)
+        }
+        /**
+         * timeToMs aceita um string contendo hh:mm:ss,MMM
+         * 
+         * Essa função pode ser passada posteriormente para uma outra biblioteca de utils
+         * Se for manter aqui, atribuir static a ela
+         */
+        timeToMs(time){
+            //Dividimos a sting do tempo separando hora, minuto e segundo, usando a expressão regular abaixo para poder dividir somente uma vez o tempo
+            let t = time.split(/[:,]/)
+            let msTime = 0;
+            t.forEach((time,key) => {
+                switch(key){
+                    case 0:
+                        msTime = (parseInt(time) * 3600000) + msTime
+                    break
+                    case 1:
+                        msTime = (parseInt(time) * 60000) + msTime
+                    break
+                    case 2:
+                        msTime = (parseInt(time) * 1000) + msTime
+                    break
+                    case 3:
+                        msTime = parseInt(time) + msTime
+                    break
+                    default:
+                        msTime = msTime + 0
+                }
+            })
+            return msTime
+        }
+        csvString(maxLines){
+            //header: index, start time, end time, duration, line1,line2,charpline1,charpline2,charpsec
+            let output = this.index + ';' + this.sTime + ';' + this.eTime + ';' + this.dur
+            let max = maxLines
+            this.lines.forEach((l,key) => {
+                output = output + ';' + l
+                max = max - 1
+            })
+            while(max > 0){
+                output = output + ';'
+                max = max - 1
+            }
+            max = maxLines
+            this.charsPerLine.forEach((c,key) => {
+                output = output + ';' + c
+                max = max - 1
+            })
+            while(max > 0){
+                output = output + ';'
+                max = max - 1
+            }
+            output = output + ";" + this.charsPerSec
+            return output
+        }
+    }
+    module.exports = SubtitleInput
